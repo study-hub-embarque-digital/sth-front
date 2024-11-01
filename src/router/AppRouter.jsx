650741e22a86559e763d9f192a7ea5a92b33178f
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-11-01 21:42:55 +0000

    feat : link

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,6 +14,7 @@ import ProfilePage from "../pages/profile_page/ProfilePage";
 import RepresentativePage from "../features/representative/RepresentativePage";
 import HomeMentor from "../features/mentor/home/HomeMentor";
 import Mentoria from "../features/mentor/mentoria/Mentoria";
+import RegisterPage from "../pages/register/RegisterPage";
 
 const AppRouter = () => {
   return (
