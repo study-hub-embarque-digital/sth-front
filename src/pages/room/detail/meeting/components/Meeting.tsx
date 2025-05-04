249cc9870a25a7a7c4781bdf5ab4bd591f6180c0
rodrigomirandaa <digoracm@gmail.com>
2025-05-04 13:29:47 -0300

    feat: jaas meeting

--- src/pages/room/detail/meeting/components/Meeting.tsx
+++ src/pages/room/detail/meeting/components/Meeting.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { JitsiMeeting } from "@jitsi/react-sdk";
+import { JaaSMeeting, JitsiMeeting } from "@jitsi/react-sdk";
 import { Box, Container, useMediaQuery } from "@mui/material";
 import { DarkTheme } from "../../../../../theme";
 import { useAuth } from "../../../../../contexts/AuthContext";
@@ -19,7 +19,7 @@ const Meeting = ({ reuniao, topicos, apresentacoes, apresentacaoAtual, discussio
   const { user } = useAuth();
 
   const meetConfigOverwrite = {
-    startWithAudioMuted: true,
+    startWithAudioMuted: false,
     disableModeratorIndicator: true,
     startScreenSharing: true,
     enableEmailInStats: false,
@@ -34,7 +34,9 @@ const Meeting = ({ reuniao, topicos, apresentacoes, apresentacaoAtual, discussio
     <Container sx={{ width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'space-evenly', padding: '10px' }
     }>
       <Box>
-        <JitsiMeeting
+        <JaaSMeeting
+          appId="vpaas-magic-cookie-5949bf32cbbe4eb082c593fa88929944"
+          jwt="eyJraWQiOiJ2cGFhcy1tYWdpYy1jb29raWUtNTk0OWJmMzJjYmJlNGViMDgyYzU5M2ZhODg5Mjk5NDQvNWJhOTJiIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJjaGF0Iiwic3ViIjoidnBhYXMtbWFnaWMtY29va2llLTU5NDliZjMyY2JiZTRlYjA4MmM1OTNmYTg4OTI5OTQ0IiwiYXVkIjoiaml0c2kiLCJleHAiOjE3NDU5MDQwMjMsImNvbnRleHQiOnsiZmVhdHVyZXMiOnsidHJhbnNjcmlwdGlvbiI6dHJ1ZSwicmVjb3JkaW5nIjp0cnVlLCJsaXZlc3RyZWFtaW5nIjpmYWxzZSwib3V0Ym91bmQtY2FsbCI6dHJ1ZSwic2lwLW91dGJvdW5kLWNhbGwiOnRydWV9LCJ1c2VyIjp7Im1vZGVyYXRvciI6dHJ1ZSwibmFtZSI6IkNsw7N2aXMgQ2hha3JpYW4iLCJpZCI6IjUxYTUwZGIxLWQ1NjctNDlmZS1iYzQ3LTc3YTE3NDNjYWE0NiIsImF2YXRhciI6bnVsbCwiZW1haWwiOiJjbG92aW5ob3NAZ21haWwuY29tIiwiaGlkZGVuLWZyb20tcmVjb3JkZXIiOmZhbHNlfSwicm9vbSI6IioifX0.jweol03_vhlbHpKvJhG6bmWlJ_ECg-0BWihRNCSXao-Bv4Yo7VqLu5WosC_fe6QG4YeqYGRVi4DumhswQzA8BUeBRDtiLevS2K5Xbi7ShqKRbQNUwhw9GV8qgDwswpdtuHZss5TIetNtLRwi2302E0T4p9lG6G4GnA9c7oHDs4oF4ti10GNAO2F4TSQCGC1Ev96orkf794KBczDhSYkgoDfaUG7Rl-famqk2ePoZ93ZaIBRZ3VjNmY1s-rlc1JpgqdL6MNEs-CLCvyvq5sTjyqtY6m_9Odk3jKQ8VxxYfBSAvqFfIZjDz-1VQvKvArihOXbPKFTSjBxfl7UFlME6LQ"
           roomName={reuniao?.reuniaoId}
           configOverwrite={meetConfigOverwrite}
           interfaceConfigOverwrite={{
