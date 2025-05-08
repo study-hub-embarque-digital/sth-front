ccef6b1a67bc1315f47306383476a77f5c39e4f8
rodrigomirandaa <digoracm@gmail.com>
2025-05-08 02:14:52 -0300

    feat: começando tela inicial admin

--- src/features/admin/AdminPage.jsx
+++ src/features/admin/AdminPage.jsx
@@ -1,4 +1,3 @@
-import BaseLayout from "../../components/shared/layout/BaseLayout";
 import { Outlet } from "react-router-dom";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import GroupsRoundedIcon from "@mui/icons-material/GroupsRounded";
@@ -8,22 +7,11 @@ import MapsHomeWorkRoundedIcon from "@mui/icons-material/MapsHomeWorkRounded";
 import BusinessCenterRoundedIcon from "@mui/icons-material/BusinessCenterRounded";
 
 export default function AdminPage() {
-  const menuItems = [
-    { text: "Home", icon: HomeRoundedIcon, route: "/admin" },
-    { text: "Squads", icon: GroupsRoundedIcon, route: "/admin/alunos" },
-    { text: "Alunos", icon: SchoolRoundedIcon, route: "/admin/alunos" },
-    { text: "Empresas", icon: MapsHomeWorkRoundedIcon, route: "/admin/alunos" },
-    { text: "I.E's", icon: AccountBalanceRoundedIcon, route: "/admin/alunos" },
-    {
-      text: "Representantes",
-      icon: BusinessCenterRoundedIcon,
-      route: "/admin/alunos",
-    },
-    { text: "I.E's", icon: AccountBalanceRoundedIcon, route: "/admin/alunos" },
-  ];
   return (
-    <BaseLayout homePath="/admin" menuItems={menuItems}>
-      <Outlet />
-    </BaseLayout>
+    <>
+    <h1>OLA</h1>
+    <Outlet/>
+    </>
+
   );
 }
