6241902200c8c88f1a12bd0d1ffee20599e289c4
rodrigomirandaa <digoracm@gmail.com>
2025-05-26 22:55:30 -0300

    feat(forms): melhora a estrutura dinâmica do formulário e a digitação dos campos

--- src/components/shared/forms/DynamicForms.tsx
+++ src/components/shared/forms/DynamicForms.tsx
@@ -1,30 +1,62 @@
 import React, { useEffect, useState } from "react";
-import { TextField, MenuItem, Checkbox, FormControlLabel, Button, Box } from "@mui/material";
+import {
+  TextField,
+  MenuItem,
+  Checkbox,
+  FormControlLabel,
+  Button,
+  Box,
+} from "@mui/material";
 import { IFormField } from "./IFormField";
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
-import ptBR from 'date-fns/locale/pt-BR';
-import { CloudUpload } from '@mui/icons-material'
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import {
+  LocalizationProvider,
+  DatePicker,
+} from "@mui/x-date-pickers";
+import ptBR from "date-fns/locale/pt-BR";
+import { CloudUpload } from "@mui/icons-material";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import { useNavigate } from "react-router-dom";
 
+
+interface Button {
+  textButton: string;
+  icon: React.ReactNode;
+  permission: boolean;
+  onClickAdd: () => void;
+}
 interface DynamicFormProps {
-  fields: IFormField[]; // Lista de campos do formulário
-  initialValues?: Record<string, any>; // Dados iniciais para edição
-  isEditable?: boolean;
-  onSubmit: (formData: Record<string, any>) => void; // Callback quando enviar
+  fields: IFormField[];
+  initialValues?: Record<string, any>;
+  onSubmit: (formData: Record<string, any>) => void;
+  hasPermission?: boolean;
+  button?: Button;
 }
 
-export const DynamicForms: React.FC<DynamicFormProps> = ({ fields, initialValues = {}, onSubmit, isEditable = true }) => {
+export const DynamicForms: React.FC<DynamicFormProps> = ({
+  fields,
+  initialValues = {},
+  onSubmit,
+  hasPermission,
+  button
+}) => {
   const [formData, setFormData] = useState<Record<string, any>>(initialValues);
+  const [editable, setEditable] = useState(!Object.keys(initialValues).length);
+  const navigate = useNavigate();
+
 
   useEffect(() => {
-    setFormData(initialValues);
-  }, []);
-  
+    if (!editable) {
+      setFormData(initialValues);
+    }
+  }, [initialValues, editable]);
 
   const getValue = (obj: any, path: string): any =>
     path.split(".").reduce((acc, key) => acc?.[key], obj);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value, type, checked } = e.target;
     const keys = name.split(".");
     const newData = { ...formData };
@@ -39,134 +71,212 @@ export const DynamicForms: React.FC<DynamicFormProps> = ({ fields, initialValues
     setFormData(newData);
   };
 
-
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSubmit(formData);
+    setEditable(false);
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center", // Centraliza tudo
-        gap: 4,
-        width: "100%",
-        maxWidth: 1000, // largura máxima
-        margin: "0 auto", // centraliza horizontalmente
-        padding: 4,
-        backgroundColor: (theme) => theme.palette.background.paper,
-        borderRadius: 2,
-        boxShadow: 3,
-      }}
-    >
+    <>
+
       <Box
+        component="form"
+        onSubmit={handleSubmit}
         sx={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-          gap: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 4,
           width: "100%",
+          maxWidth: 1000,
+          margin: "0 auto",
+          padding: 4,
+          backgroundColor: (theme) => theme.palette.background.paper,
+          borderRadius: 2,
+          boxShadow: 3,
         }}
       >
-        {fields.map((field) => (
-          <Box key={field.name}>
-            {field.type === "text" || field.type === "number" || field.type === 'password' ? (
-              <TextField
-                color="secondary"
-                fullWidth
-                label={field.label}
-                type={field.type}
-                name={field.name}
-                value={getValue(formData, field.name) || ""}
-                onChange={handleChange}
-                required={field.required}
-                disabled={!isEditable}
-                sx={{
-                  "& .MuiInputBase-root.Mui-disabled": {
-                    backgroundColor: "#f0f0f0",
-                  },
-                  "& .MuiInputBase-input.Mui-disabled": {
-                    color: "#000",
-                    WebkitTextFillColor: "#000",
-                    opacity: 1,
-                  },
-                  "& .MuiInputLabel-root.Mui-disabled": {
-                    color: "#000",
-                    opacity: 1,
-                  },
-                }}
-              />
-            ) : field.type === "select" ? (
-              <TextField
-                color="secondary"
-                select
-                fullWidth
-                label={field.label}
-                name={field.name}
-                value={getValue(formData, field.name) || ""}
-                onChange={handleChange}
-                required={field.required}
-                disabled={!isEditable}
-              >
-                {field.options?.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            ) : field.type === "checkbox" ? (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={getValue(formData, field.name) || false}
-                    onChange={handleChange}
-                    name={field.name}
-                    disabled={!isEditable}
-                  />
-                }
-                label={field.label}
-              />
-            ) : (
-              <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
-                <DatePicker
+        <Box sx={{ display: "flex", justifyContent: "flex-end", width: '100%', gap: 2 }}>
+          {button && (
+            <Button
+              variant="contained"
+              size="medium"
+              color="secondary"
+              endIcon={button.icon}
+              onClick={button.onClickAdd}
+            >
+              {button.textButton}
+            </Button>
+          )}
+          {hasPermission && Object.keys(initialValues).length > 0 && (
+            <Button
+              variant="contained"
+              size="medium"
+              color="secondary"
+              endIcon={<EditRoundedIcon />}
+              onClick={() => setEditable((prev) => !prev)}
+            >
+              {editable ? "Cancelar" : "Editar"}
+            </Button>
+          )}
+
+        </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+            gap: 3,
+            width: "100%",
+          }}
+        >
+          {fields.map((field) => (
+            <Box key={field.name}>
+              {field.type === "text" ||
+                field.type === "number" ||
+                field.type === "password" ? (
+                <TextField
+                  color="secondary"
+                  fullWidth
                   label={field.label}
-                  value={getValue(formData, field.name) || null}
-                  onChange={(newValue) => {
-                    if (!isEditable) return;
-                    setFormData({
-                      ...formData,
-                      [field.name]: newValue,
-                    });
-                  }}
-                  format="dd/MM/yyyy"
-                  slotProps={{
-                    textField: {
-                      color: "secondary",
-                      disabled: !isEditable,
-                      fullWidth: true,
+                  type={field.type}
+                  name={field.name}
+                  value={getValue(formData, field.name) ?? ""}
+                  onChange={handleChange}
+                  required={field.required}
+                  disabled={!editable || field.disabledOnEdit}
+                  sx={{
+                    "& .MuiInputBase-root.Mui-disabled": {
+                      backgroundColor: "#f0f0f0",
+                    },
+                    "& .MuiInputBase-input.Mui-disabled": {
+                      color: "#000",
+                      WebkitTextFillColor: "#000",
+                      opacity: 1,
+                    },
+                    "& .MuiInputLabel-root.Mui-disabled": {
+                      color: "#000",
+                      opacity: 1,
                     },
                   }}
                 />
-              </LocalizationProvider>
-            )}
+              ) : field.type === "select" ? (
+                <TextField
+                  color="secondary"
+                  select
+                  fullWidth
+                  label={field.label}
+                  name={field.name}
+                  value={getValue(formData, field.name) ?? ""}
+                  onChange={handleChange}
+                  required={field.required}
+                  disabled={!editable || field.disabledOnEdit}
+                  sx={{
+                    "& .MuiInputBase-root.Mui-disabled": {
+                      backgroundColor: "#f0f0f0",
+                    },
+                    "& .MuiInputBase-input.Mui-disabled": {
+                      color: "#000",
+                      WebkitTextFillColor: "#000",
+                      opacity: 1,
+                    },
+                    "& .MuiInputLabel-root.Mui-disabled": {
+                      color: "#000",
+                      opacity: 1,
+                    },
+                  }}
+                >
+                  {field.options?.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              ) : field.type === "checkbox" ? (
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={getValue(formData, field.name) ?? false}
+                      onChange={handleChange}
+                      name={field.name}
+                      disabled={!editable || field.disabledOnEdit}
+                      sx={{
+                        "& .MuiInputBase-root.Mui-disabled": {
+                          backgroundColor: "#f0f0f0",
+                        },
+                        "& .MuiInputBase-input.Mui-disabled": {
+                          color: "#000",
+                          WebkitTextFillColor: "#000",
+                          opacity: 1,
+                        },
+                        "& .MuiInputLabel-root.Mui-disabled": {
+                          color: "#000",
+                          opacity: 1,
+                        },
+                      }}
+                    />
+                  }
+                  label={field.label}
+                />
+              ) : (
+                <LocalizationProvider
+                  dateAdapter={AdapterDateFns}
+                  adapterLocale={ptBR}
+                >
+                  <DatePicker
+                    label={field.label}
+                    maxDate={new Date()}
+                    value={
+                      getValue(formData, field.name)
+                        ? new Date(getValue(formData, field.name))
+                        : null
+                    }
+                    onChange={(newValue) => {
+                      if (!editable) return;
+                      setFormData({
+                        ...formData,
+                        [field.name]: newValue,
+                      });
+                    }}
+                    format="dd/MM/yyyy"
+                    slotProps={{
+                      textField: {
+                        helperText: 'DD/MM/YYYY',
+                        color: "secondary",
+                        disabled: !editable || field.disabledOnEdit,
+                        fullWidth: true,
+                      },
+
+                    }}
+                  />
+                </LocalizationProvider>
+              )}
+            </Box>
+          ))}
+        </Box>
+
+        {editable && (
+          <Box sx={{ display: "flex", gap: 2, width: "100%" }}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onClick={() => {
+                if (Object.keys(initialValues).length === 0) {
+                  navigate(-1);
+                }
+                setFormData(initialValues);
+                setEditable(false);
+              }}
+            >
+              Cancelar
+            </Button>
+            <Button type="submit" variant="contained" color="secondary" fullWidth>
+              Salvar
+            </Button>
           </Box>
-        ))}
+        )}
       </Box>
-
-      {isEditable && (
-        <Button
-          fullWidth
-          type="submit"
-          variant="contained"
-          color="primary"
-        >
-          Enviar
-        </Button>
-      )}
-    </Box>
+    </>
   );
-
 };
